06b82c7e8161312b00ce1496284402c43b2f32d8
Rishabh Singh <rishabh.singh.prof12@gmail.com>
2026-01-30 17:42:14 +0000

    Second class coding exercises

---
+++ Second_class/midLevelIfElse.py
@@ -0,0 +1,21 @@
+a = int(input("Enter your first number = "))
+b = int(input("Enter your second number = "))
+c = a * b
+
+print("Product: ", c)
+
+# Check odd/even
+if c % 2 != 0:
+    square = c**2
+    print("Square of the product =", square)
+elif c % 2 == 0:
+    cube = c**3
+    print("Cube of the product =", cube)
+
+# Check range
+if c > 50 and c < 200:
+    squareroot = c**(1/2)
+    print("Square root of the product =", squareroot)
+elif c > 200:
+    cuberoot = c**(1/3)
+    print("Cube root of the product =", cuberoot)
