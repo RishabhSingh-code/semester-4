f31b3eef4dd2a182625116744455841e17f34a72
Rishabh Singh <rishabh.singh.prof12@gmail.com>
2026-02-18 21:17:31 +0000

    symbol pattern problem

---
+++ Pattern_Problems/Square_fill_pattern.py
@@ -0,0 +1,8 @@
+n= 5
+for i in range(n):
+    for j in range(n):
+        print('$',end ="  ")
+    print()
+    ''' the last print statement adds new line for i
+      so after j is executed for the first line i 
+      is executed '''
